540aa37427ec83858105fcb2d397b05181b01c91
MikeNkunku <mikenkunku@gmail.com>
2016-02-03 17:10:51 +0100

    Defined all functions

---
+++ server/app/controllers/ProductionTypeController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Controllers;
+
+use Phalcon\Exception;
+use BaseController;
+use Models\ProductionType;
+
+class ProductionTypeController extends BaseController {
+	/**
+	 * @param integer $productionTypeId
+	 */
+	public function get($productionTypeId) {
+		if (!$this->application->request->isGet()) {
+			throw new Exception('Method not allowed', 405);
+		}
+
+		$pt = ProductionType::findFirst($productionTypeId);
+		if (!$pt) {
+			throw new Exception('ProductionType not found', 404);
+		}
+
+		return array('code' => 200, 'content' => $pt->toArray());
+	}
+
+	public function add() {
+		if (!$this->application->request->isPost()) {
+			throw new Exception('Method not allowed', 405);
+		}
+
+		if (!$this->isAllowed()) {
+			throw new Exception('User not authorized', 401);
+		}
+
+		$postData = $this->application->request->getJsonRawBody();
+
+		$pt = ProductionType::findFirst(array('name' => $postData->name));
+		if ($pt) {
+			throw new Exception('ProductionType already created', 409);
+		}
+
+		$pt = new ProductionType();
+		$create = $pt->create(array('name' => $postData->name));
+
+		if (!$create) {
+			throw new Exception('ProductionType not created', 409);
+		}
+
+		return array('code' => 201, 'content' => $pt->toArray());
+	}
+
+	/**
+	 * @param integer $productionTypeId
+	 */
+	public function delete($productionTypeId) {
+		if (!$this->application->request->isDelete()) {
+			throw new Exception('Method not allowed', 405);
+		}
+
+		if (!$this->isAllowed()) {
+			throw new Exception('User not authorized', 401);
+		}
+
+		$pt = ProductionType::findFirst($productionTypeId);
+		if (!$pt) {
+			throw new Exception('ProductionType not found', 404);
+		}
+
+		if (!$pt->delete()) {
+			throw new Exception('ProductionType not deleted', 409);
+		}
+
+		return array('code' => 204, 'content' => 'ProductionType deleted');
+	}
+}
