d922c24d0988fa9a6b0bc7da62719ca336998ff1
MikeNkunku <mikenkunku@gmail.com>
2016-02-08 22:24:23 +0100

    Defined all routes

---
+++ server/app/collections/AnimeCollection.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Collections;
+
+use \Phalcon\Mvc\Micro\Collection as MicroCollection;
+
+$animeCollection = new MicroCollection();
+$animeCollection->setHandler('Controllers\AnimeController');
+$animeCollection->setPrefix('/api/collections');
+
+$animeCollection->get('/{animeId:[0-9]+}', 'get');
+$animeCollection->post('/', 'add');
+$animeCollection->put('/{animeId:[0-9]+}', 'update');
+$animeCollection->delete('/{animeId:[0-9]+}', 'delete');
+
+$animeCollection->get('/all', 'getAll');
+$animeCollection->get('/list', 'getValidList');
+$animeCollection->post('/list/{statusName:w+}', 'getList');
+
+return $animeCollection;
