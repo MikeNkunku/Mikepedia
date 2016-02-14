9406a5f4d6c198c19d4234c4df92bec807dd79c1
MikeNkunku <mikenkunku@gmail.com>
2016-02-14 08:33:31 +0100

    Added update function

--- server/app/controllers/CelebrityController.php
+++ server/app/controllers/CelebrityController.php
@@ -95,4 +95,68 @@ class CelebrityController extends BaseController {
 
 		return array('code' => 201, 'content' => array_merge($c->toArray(), $pArr));
 	}
+
+	/**
+	 * @param integer $celebrityId
+	 */
+	public function update($celebrityId) {
+		if (!$this->application->request->isPut()) {
+			throw new Exception('Method not allowed', 405);
+		}
+
+		if (!$this->isAllowed()) {
+			throw new Exception('User not authorized', 401);
+		}
+
+		$c = Celebrity::findFirst($celebrityId);
+		if (!$c) {
+			throw new Exception('Celebrity not found', 404);
+		}
+
+		$putData = $this->application->request->getJsonRawBody();
+		if (empty($putData->statusId)) {
+			throw new Exception('Status ID field must not be empty', 409);
+		}
+		if (empty($putData->firstname)) {
+			throw new Exception('Firstname field cannot be empty', 409);
+		}
+		if (empty($putData->lastname)) {
+			throw new Exception('Lastname field cannot be empty', 409);
+		}
+		if (empty($putData->gender)) {
+			throw new Exception('Gender field must not be empty', 409);
+		}
+		if (empty($putData->birthdate)) {
+			throw new Exception('Birthdate field must not be empty', 409);
+		}
+		if (empty($putData->summary)) {
+			throw new Exception('Summary field must not be empty', 409);
+		}
+		if (empty($putData->biography)) {
+			throw new Exception('Biography field cannot be empty', 409);
+		}
+		if (empty($putData->picture)) {
+			throw new Exception('Picture field must be filled', 409);
+		}
+
+		$update = $c->beforeUpdate($putData);
+		if (!$update) {
+			throw new Exception('Parent class not updated', 409);
+		}
+
+		if (empty($putData->earlyLife)) {
+			throw new Exception('EarlyLife field must not be empty', 409);
+		}
+		$update = $c->update(array('early_life' => $putData->earlyLife));
+
+		if (!$update) {
+			throw new Exception('Celebrity not updated', 409);
+		}
+
+		$p = Person::findFirst($c->getPersonId());
+		$pArr = $p->toArray();
+		unset($pArr['id']);
+
+		return array('code' => 200, 'content' => array_merge($c->toArray, $pArr));
+	}
 }
