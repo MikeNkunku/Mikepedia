b2b7f7d29a89f18c2df652252259318983cc1d61
MikeNkunku <mikenkunku@gmail.com>
2016-02-15 22:42:08 +0100

    Defined all routes

---
+++ server/app/collections/EpisodeCollection.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Collections;
+
+use \Phalcon\Mvc\Micro\Collection as MicroCollection;
+
+$episodeCollection = new MicroCollection();
+$episodeCollection->setHandler('Controllers\EpisodeController');
+$episodeCollection->setPrefix('/api/episodes');
+
+$episodeCollection->get('/{episodeId:[0-9]+}', 'get');
+$episodeCollection->post('/', 'add');
+$episodeCollection->put('/{episodeId:[0-9]+}', 'update');
+$episodeCollection->delete('/{episodeId:[0-9]+}', 'delete');
+
+$episodeCollection->get('/all', 'getAll');
+$episodeCollection->get('/list', 'getValidList');
+$episodeCollection->post('/list/{statusName:w+}', 'getList');
+
+return $episodeCollection;
