b68a583ce07d5f1e25f002b564ddb2932950ab7e
MikeNkunku <mikenkunku@gmail.com>
2016-02-15 22:53:07 +0100

    Declared all functions to consider

---
+++ server/app/controllers/GameController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Controllers;
+
+use Phalcon\Exception;
+
+use BaseController;
+use Models\Status;
+use Models\Game;
+use Models\GameGenre;
+use Models\GamePlatform;
+use Models\GameSection;
+use Models\GameSubsection;
+
+class GameController extends BaseController {
+	/**
+	 * @param integer $gameId
+	 */
+	public function get($gameId) {
+
+	}
+
+	public function add() {
+
+	}
+
+	/**
+	 * @param integer $gameId
+	 */
+	public function update($gameId) {
+
+	}
+
+	/**
+	 * @param integer $gameId
+	 */
+	public function delete($gameId) {
+
+	}
+
+	public function getAll() {
+
+	}
+
+	public function getValidList() {
+
+	}
+
+	/**
+	 * @param text $statusName
+	 */
+	public function getList($statusName) {
+
+	}
+
+	/**
+	 * @param integer $gameGenreId
+	 */
+	public function getByGenre($gameGenreId) {
+
+	}
+
+	/**
+	 * @param integer $gamePlatformId
+	 */
+	public function getByPlatform($gamePlatformId) {
+
+	}
+}
