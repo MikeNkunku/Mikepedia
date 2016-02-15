d006f087fec51bfaf4b07773e2507291fc077925
MikeNkunku <mikenkunku@gmail.com>
2016-02-15 05:49:32 +0100

    Added update function

--- server/app/controllers/EpisodeController.php
+++ server/app/controllers/EpisodeController.php
@@ -93,4 +93,66 @@ class EpisodeController extends BaseController {
 
 		return array('code' => 201, 'content' => array_merge($eArr, $seasonNumber, $bpName));
 	}
+
+	/**
+	 * @param integer $episodeId
+	 */
+	public function update($episodeId) {
+		if (!$this->application->request->isPut()) {
+			throw new Exception('Method not allowed', 405);
+		}
+
+		if (!$this->isAllowed()) {
+			throw new Exception('User not authorized', 401);
+		}
+
+		$episode = Episode::findFirst($episodeId);
+		if (!$episode) {
+			throw new Exception('Episode not found', 404);
+		}
+
+		$putData = $this->application->request->getJsonRawBody();
+		if (empty($putData->statusId)) {
+			throw new Exception('Status ID cannot be null', 409);
+		}
+		if (empty($putData->seasonId)) {
+			throw new Exception('Season ID cannot be null', 409);
+		}
+		if (empty($putData->number)) {
+			throw new Exception('Number must not be empty', 409);
+		}
+		if (empty($putData->summary)) {
+			throw new Exception('Summary field cannot be empty', 409);
+		}
+		if (empty($putData->description)) {
+			throw new Exception('Description field cannot be empty', 409);
+		}
+
+		$episode->beforeUpdate();
+		if (!empty($putData->airedAt) && ($putData->airedAt == date('Y-m-d', $episode->getAiredAt()))) {
+			$episode->setAiredAt(date_create_from_format('Y-m-d', $putData->airedAt, new \DateTimeZone('UTC')));
+		}
+		$update = $episode->update(array(
+				'status_id' => $putData->statusId,
+				'summary' => $putData->summary,
+				'description' => $putData->description,
+				'number' => $putData->number,
+				'season_id' => $putData->seasonId
+		));
+		if (!$update) {
+			throw new Exception('Episode not updated', 409);
+		}
+
+		$season = Season::findFirst($episode->getSeasonId());
+		$seasonNumber = $season->getNumber();
+
+		$BP = BroadcastProgram::findFirst($season->getProgramId());
+		$bpName = $BP->getName();
+
+		$eArr = $episode->toArray();
+		$eArr['created_at'] = date('Y-m-d H:i:sP', $eArr['created_at']);
+		$eArr['updated_at'] = date('Y-m-d H:i:sP', $eArr['updated_at']);
+
+		return array('code' => 200, 'content' => array_merge($eArr, $seasonNumber, $bpName));
+	}
 }
