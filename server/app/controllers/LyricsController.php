e5b9fc806f013a7fad1e226c5709051c8149fb79
MikeNkunku <mikenkunku@gmail.com>
2016-02-26 17:03:38 +0100

    Added add function

--- server/app/controllers/LyricsController.php
+++ server/app/controllers/LyricsController.php
@@ -5,8 +5,6 @@ namespace Controllers;
 use Phalcon\Exception;
 use BaseController;
 use Models\Lyrics;
-use Models\Song;
-use Models\Celebrity;
 
 class LyricsController extends BaseController {
 	/**
@@ -27,5 +25,43 @@ class LyricsController extends BaseController {
 		$lArr['updated_at'] = date('Y-m-d H:i:sP', $lArr['updated_at']);
 
 		return array('code' => 200, 'content' => $lArr);
+	}
+
+	public function add() {
+		if (!$this->application->request->isPost()) {
+			throw new Exception('Method not allowed', 405);
+		}
+
+		if (!$this->isAllowed()) {
+			throw new Exception('User not authorized', 401);
+		}
+
+		$postData = $this->application->request->getJsonRawBody();
+		if (empty($postData->statusId)) {
+			throw new Exception('Status ID cannot be null', 409);
+		}
+		if (empty($postData->songId)) {
+			throw new Exception('Lyrics must be associated with a song', 409);
+		}
+		if (empty($postData->content)) {
+			throw new Exception('Content field must be filled', 409);
+		}
+
+		$lyrics = new Lyrics();
+		$lyrics->beforeCreate();
+		$create = $lyrics->create(array(
+			'song_id' => $postData->songId,
+			'status_id' => $postData->statusId,
+			'content' => $postData->Content
+		));
+		if (!$create) {
+			throw new Exception('Lyrics instance not created', 409);
+		}
+
+		$lArr = $lyrics->toArray();
+		$lArr['created_at'] = date('Y-m-d H:i:sP', $lArr['created_at']);
+		$lArr['updated_at'] = date('Y-m-d H:i:sP', $lArr['updated_at']);
+
+		return array('code' => 201, 'content' => $lArr);
 	}
 }
