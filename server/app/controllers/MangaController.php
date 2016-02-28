53a1675bd8f0bdb77e95d32a579a0a032db98a06
MikeNkunku <mikenkunku@gmail.com>
2016-02-28 19:41:52 +0100

    Added updated function

--- server/app/controllers/MangaController.php
+++ server/app/controllers/MangaController.php
@@ -83,4 +83,60 @@ class MangaController extends BaseController {
 
 		return array('code' => 201, 'content' => $manga->toArray());
 	}
+
+	/**
+	* @param integer $mangaId
+	*/
+	public function update($mangaId) {
+		if (!$this->application->request->isPut()) {
+			throw new Exception('Method not allowed', 405);
+		}
+		if (!$this->isAllowed()) {
+			throw new Exception('User not authorized', 401);
+		}
+
+		$manga = Manga::findFirst($mangaId);
+		if (!$manga) {
+			throw new Exception('Manga instance not found', 404);
+		}
+
+		$putData = $this->application->request->getJsonRawBody();
+		if (empty($putData->name)) {
+			throw new Exception('Name field cannot be empty', 409);
+		}
+		if (empty($putData->statusId)) {
+			throw new Exception('Status ID cannot be null', 409);
+		}
+		if (empty($putData->creatorId)) {
+			throw new Exception('Creator ID field must be filled', 409);
+		}
+		if (empty($putData->hasAnime)) {
+			throw new Exception('HasAnime field must be filled', 409);
+		}
+		if (empty($putData->year)) {
+			throw new Exception('Year field must be filled', 409);
+		}
+		if (empty($putData->genres)) {
+			throw new Exception('Genres field must at least contains one element', 409);
+		}
+		if (empty($putData->demographyId)) {
+			throw new Exception('Demography ID cannot be empty', 409);
+		}
+
+		$manga->beforeUpdate();
+		$update = $manga->update(array(
+			'name' => $putData->name,
+			'status_id' => $putData->statusId,
+			'creator_id' => $putData->creatorId,
+			'has_anime' => $putData->hasAnime,
+			'year' => $putData->year,
+			'genres' => $putData->genres,
+			'demography_id' => $putData->demographyId
+		));
+		if (!$update) {
+			throw new Exception('Manga instance not updated', 409);
+		}
+
+		return array('code' => 200, 'content' => $manga->toArray());
+	}
 }
