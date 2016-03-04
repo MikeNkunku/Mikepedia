af89cd5f3e734db0b9d878e5d45175839f474742
MikeNkunku <mikenkunku@gmail.com>
2016-03-04 18:22:25 +0100

    Deleted useless setters & Formatted timestamp return for getCreatedAt & getUpdatedAt functions

--- server/app/models/Movie.php
+++ server/app/models/Movie.php
@@ -191,27 +191,13 @@ class Movie extends Model {
 	 * @return timestamp
 	 */
 	public function getCreatedAt() {
-		return $this->created_at;
-	}
-
-	/**
-	 * @param timestamp $created_at
-	 */
-	public function setCreatedAt($created_at) {
-		$this->created_at = $created_at;
+		return date('Y-m-d H:i:sP', $this->created_at);
 	}
 
 	/**
 	 * @return timestamp
 	 */
 	public function getUpdatedAt() {
-		return $this->updated_at;
-	}
-
-	/**
-	 * @param timestamp $updated_at
-	 */
-	public function setUpdatedAt($updated_at) {
-		$this->updated_at = $updated_at;
+		return date('Y-m-d H:i:sP', $this->updated_at);
 	}
 }
