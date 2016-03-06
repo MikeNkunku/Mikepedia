858b6803667eb5f9a8b3d11d754215da2abc0133
MikeNkunku <mikenkunku@gmail.com>
2016-03-06 18:38:59 +0100

    Added get function

--- server/app/controllers/MangaCharacterController.php
+++ server/app/controllers/MangaCharacterController.php
@@ -13,5 +13,23 @@ use Models\BroadcastProgram;
 use Models\Anime;
 
 class MangaCharacterController extends BaseController {
+	/**
+	* @param integer $mangaCharacterId
+	*/
+	public function get($mangaCharacterId) {
+		if (!$this->application->request->isGet()) {
+			throw new Exception('Method not allowed', 405);
+		}
 
+		$mc = MangaCharacter::findFirst($mangaCharacterId);
+		if (!$mc) {
+			throw new Exception('MangaCharacter instance not found', 404);
+		}
+
+		$p = Person::findFirst($mc->getPersonId());
+		$pArr = $p->toArray();
+		unset($pArr['id']);
+
+		return array('code' => 200, 'content' => array_merge($mc->toArray(), $pArr));
+	}
 }
