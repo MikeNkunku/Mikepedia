8a77238dceb3910fcb5e560703526574311174fc
MikeNkunku <mikenkunku@gmail.com>
2016-03-06 19:07:54 +0100

    Added update function

--- server/app/controllers/MangaCharacterController.php
+++ server/app/controllers/MangaCharacterController.php
@@ -94,4 +94,73 @@ class MangaCharacterController extends BaseController {
 
 		return array('code' => 201, 'content' => array_merge($mc->toArray(), $pArr));
 	}
+
+	/**
+	* @param integer $mangaCharacterId
+	*/
+	public function update($mangaCharacterId) {
+		if (!$this->application->request->isPut()) {
+			throw new Exception('Method not allowed', 405);
+		}
+		if (!$this->isAllowed()) {
+			throw new Exception('User not authorized', 401);
+		}
+
+		$mc = MangaCharacter::findFirst($mangaCharacterId);
+		if (!$mc) {
+			throw new Exception('MangaCharacter instance not found', 404);
+		}
+
+		$putData = $this->application->request->getJsonRawBody();
+		if (empty($putData->statusId)) {
+			throw new Exception('Status ID field must not be empty', 409);
+		}
+		if (empty($putData->firstname)) {
+			throw new Exception('Firstname field cannot be empty', 409);
+		}
+		if (empty($putData->lastname)) {
+			throw new Exception('Lastname field cannot be empty', 409);
+		}
+		if (empty($putData->gender)) {
+			throw new Exception('Gender field must not be empty', 409);
+		}
+		if (empty($putData->birthdate)) {
+			throw new Exception('Birthdate field must not be empty', 409);
+		}
+		if (empty($putData->summary)) {
+			throw new Exception('Summary field must not be empty', 409);
+		}
+		if (empty($putData->biography)) {
+			throw new Exception('Biography field cannot be empty', 409);
+		}
+		if (empty($putData->picture)) {
+			throw new Exception('Picture field must be filled', 409);
+		}
+		if (empty($putData->mangaId)) {
+			throw new Exception('Manga ID field cannot be null', 409);
+		}
+		if (empty($putData->personality)) {
+			throw new Exception('Personality attribute must be filled', 409);
+		}
+
+		$preUpdate = $mc->beforeUpdate($putData);
+		if (!$preUpdate) {
+			throw new Exception('Parent class not updated', 409);
+		}
+
+		$update = $mc->update(array(
+			'manga_id' => $putData->mangaId,
+			'personality' => $putData->personality,
+			'person_id' => $putData->personId
+		));
+		if (!$update) {
+			throw new Exception('MangaCharacter instance not updated', 409);
+		}
+
+		$p = Person::findFirst($mc->getPersonId());
+		$pArr = $p->toArray();
+		unset($pArr['id']);
+
+		return array('code' => 200, 'content' => array_merge($mc->toArray(), $pArr));
+	}
 }
