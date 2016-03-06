6f5438e9fc58dcf50d51e28306a54716d2602cd3
MikeNkunku <mikenkunku@gmail.com>
2016-03-06 18:33:10 +0100

    Created MangaCharacterController

---
+++ server/app/controllers/MangaCharacterController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Controllers;
+
+use Phalcon\Exception;
+
+use BaseController;
+use Models\Person;
+use Models\MangaCharacter;
+use Models\Status;
+use Models\Manga;
+use Models\BroadcastProgram;
+use Models\Anime;
+
+class MangaCharacterController extends BaseController {
+
+}
