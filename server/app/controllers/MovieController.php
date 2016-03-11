6c38eca5be70b4eb3342b83ebbe46d67f3c1f62d
MikeNkunku <mikenkunku@gmail.com>
2016-03-11 18:51:16 +0100

    Added get function

--- server/app/controllers/MovieController.php
+++ server/app/controllers/MovieController.php
@@ -9,5 +9,19 @@ use Models\Status;
 use Models\Movie;
 
 class MovieController extends BaseController {
+	/**
+	* @param integer $movieId
+	*/
+	public function get($movieId) {
+		if (!$this->application->request->isGet()) {
+			throw new Exception('Method not allowed', 405);
+		}
 
+		$movie = Movie::findFirst($movieId);
+		if (!$movie) {
+			throw new Exception('Movie instance not found', 404);
+		}
+
+		return array('code' => 200, 'content' => $movie->toArray());
+	}
 }
