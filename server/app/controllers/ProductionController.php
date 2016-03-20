3b6456ed1b0c1fd12b0568e287095d1b8e022a75
MikeNkunku <mikenkunku@gmail.com>
2016-03-20 20:14:31 +0100

    Added update function

--- server/app/controllers/ProductionController.php
+++ server/app/controllers/ProductionController.php
@@ -69,4 +69,56 @@ class ProductionController extends BaseController {
 
 		return array('code' => 201, 'content' => $production->toArray());
 	}
+
+	/**
+	* @param integer $productionId
+	*/
+	public function update($productionId) {
+		if (!$this->application->request->isPut()) {
+			throw new Exception('Method not allowed', 405);
+		}
+		if (!$this->isAllowed()) {
+			throw new Exception('User not authorized', 401);
+		}
+
+		$production = Production::findFirst($productionId);
+		if (!$production) {
+			throw new Exception('Production instance not found', 404);
+		}
+
+		$putData = $this->application->request->getJsonRawBody();
+		if (empty($putData->typeId)) {
+			throw new Exception('Type ID field cannot be null', 409);
+		}
+		if (empty($putData->statusId)) {
+			throw new Exception('Status ID field cannot be null', 409);
+		}
+		if (empty($putData->name)) {
+			throw new Exception('Name field cannot be empty', 409);
+		}
+		if (empty($putData->artistId)) {
+			throw new Exception('Artist ID field must be filled', 409);
+		}
+		if (empty($putData->releaseDate)) {
+			throw new Exception('Release date attribute cannot be null', 409);
+		}
+		if (empty($putData->summary)) {
+			throw new Exception('Summary field must be filled', 409);
+		}
+
+		$production->beforeUpdate();
+		$update = $production->update(array(
+			'type_id' => $putData->typeId,
+			'status_id' => $putData->statusId,
+			'name' => $putData->name,
+			'artist_id' => $putData->artistId,
+			'release_date' => $putData->releaseDate,
+			'summary' => $putData->summary
+		));
+		if (!$update) {
+			throw new Exception('Production instance not updated', 500);
+		}
+
+		return array('code' => 200, 'content' => $production->toArray());
+	}
 }
