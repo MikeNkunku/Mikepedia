8ee99a450193454ffc9e9544c557eeb83cdc629d
MikeNkunku <mikenkunku@gmail.com>
2016-03-20 20:11:03 +0100

    Added add function

--- server/app/controllers/ProductionController.php
+++ server/app/controllers/ProductionController.php
@@ -24,4 +24,49 @@ class ProductionController extends BaseController {
 
 		return array('code' => 200, 'content' => $production->toArray());
 	}
+
+	public function add() {
+		if (!$this->application->request->isPost()) {
+			throw new Exception('Method not allowed', 405);
+		}
+		if (!$this->isAllowed()) {
+			throw new Exception('User not authorized', 401);
+		}
+
+		$postData = $this->application->request->getJsonRawBody();
+		if (empty($postData->typeId)) {
+			throw new Exception('Type ID field cannot be null', 409);
+		}
+		if (empty($postData->statusId)) {
+			throw new Exception('Status ID field cannot be null', 409);
+		}
+		if (empty($postData->name)) {
+			throw new Exception('Name field cannot be empty', 409);
+		}
+		if (empty($postData->artistId)) {
+			throw new Exception('Artist ID field must be filled', 409);
+		}
+		if (empty($postData->releaseDate)) {
+			throw new Exception('Release date attribute cannot be null', 409);
+		}
+		if (empty($postData->summary)) {
+			throw new Exception('Summary field must be filled', 409);
+		}
+
+		$production = new Production();
+		$production->beforeCreate();
+		$create = $production->create(array(
+			'type_id' => $postData->typeId,
+			'status_id' => $postData->statusId,
+			'name' => $postData->name,
+			'artist_id' => $postData->artistId,
+			'release_date' => $postData->releaseDate,
+			'summary' => $postData->summary
+		));
+		if (!$create) {
+			throw new Exception('Production instance not created', 500);
+		}
+
+		return array('code' => 201, 'content' => $production->toArray());
+	}
 }
