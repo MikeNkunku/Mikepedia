f65d4e7de75fb58073d70952c801c60ee15428e9
MikeNkunku <mikenkunku@gmail.com>
2016-03-26 06:38:24 +0100

    Added update function

--- server/app/controllers/SongController.php
+++ server/app/controllers/SongController.php
@@ -65,4 +65,51 @@ class SongController extends BaseController {
 
 		return array('code' => 201, 'content' => $song->toArray());
 	}
+
+	/**
+	 * @param integer $songId
+	 */
+	public function update($songId) {
+		if (!$this->application->request->isPut()) {
+			throw new Exception('Method not allowed', 405);
+		}
+		if (!$this->isAllowed()) {
+			throw new Exception('User not authorized', 401);
+		}
+
+		$song = Song::findFirst($songId);
+		if (!$song) {
+			throw new Exception('Song instance not found', 404);
+		}
+
+		$putData = $this->application->request->getJsonRawBody();
+		if (empty($putData->productionId)) {
+			throw new Exception('Production ID attribute cannot be null', 409);
+		}
+		if (empty($putData->genreId)) {
+			throw new Exception('Status ID attribute cannot be null', 409);
+		}
+		if (empty($putData->title)) {
+			throw new Exception('Song instance must have a non-empty title', 409);
+		}
+		if (empty($putData->statusId)) {
+			throw new Exception('Status ID attribute cannot be null', 409);
+		}
+
+		$song->beforeUpdate();
+		if (!empty($putData->number)) {
+			$song->setNumber($putData->number);
+		}
+		$update = $song->update(array(
+			'production_id' => $putData->productionId,
+			'genre_id' => $putData->genreId,
+			'title' => $putData->title,
+			'status_id' => $putData->statusId
+		));
+		if (!$update) {
+			throw new Exception('Song instance not updated', 500);
+		}
+
+		return array('code' => 200, 'content' => $song->toArray());
+	}
 }
