b663990d52ff93fd4830ec52e8c1b20123ca87f9
MikeNkunku <mikenkunku@gmail.com>
2016-03-27 20:01:33 +0200

    Updated previous routes + Added a new one

--- server/app/collections/AnimeCollection.php
+++ server/app/collections/AnimeCollection.php
@@ -15,6 +15,7 @@ $animeCollection->delete('/{animeId:[0-9]+}', 'delete');
 
 $animeCollection->get('/all', 'getAll');
 $animeCollection->get('/list', 'getValidList');
-$animeCollection->post('/list/{statusName:w+}', 'getList');
+$animeCollection->post('/list/{statusName:[a-z]+}', 'getList');
+$animeCollection->get('/{animeId:[0-9]+}/seasons', 'getSeasons');
 
 return $animeCollection;
