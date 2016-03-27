9dc1e01bbb4c947fea4dcbe946a3e6228d9fd122
MikeNkunku <mikenkunku@gmail.com>
2016-03-27 12:13:02 +0200

    Added update function

--- server/app/controllers/TVSeriesController.php
+++ server/app/controllers/TVSeriesController.php
@@ -82,4 +82,60 @@ class TVSeriesController extends BaseController {
 
 		return array('code' => 201, 'content' => array_merge($tvSeries->toArray(), $bpArr));
 	}
+
+	/**
+	 * @param integer $tvseriesId
+	 */
+	public function update($tvseriesId) {
+		if (!$this->application->request->isPut()) {
+			throw new Exception('Method not allowed', 405);
+		}
+		if (!$this->isAllowed()) {
+			throw new Exception('User not authorized', 401);
+		}
+
+		$tvSeries = TVSeries::findFirst($tvseriesId);
+		if (!$tvseriesId) {
+			throw new Exception('TVSeries instance not found', 404);
+		}
+
+		$putData = $this->application->request->getJsonRawBody();
+		if (empty($putData->typeId)) {
+			throw new Exception('Type ID field cannot be null', 409);
+		}
+		if (empty($putData->name)) {
+			throw new Exception('Name field must be filled', 409);
+		}
+		if (empty($putData->startDate)) {
+			throw new Exception('Name field must be filled', 409);
+		}
+		if (empty($putData->summary)) {
+			throw new Exception('Name field must be filled', 409);
+		}
+		if (empty($putData->statusId)) {
+			throw new Exception('Status ID field cannot be null', 409);
+		}
+
+		$preUpdate = $tvSeries->beforeUpdate();
+		if (!$preUpdate) {
+			throw new Exception('Parent class not updated', 500);
+		}
+
+		if (empty($putData->mainCast)) {
+			throw new Exception('Main cast field must be filled', 409);
+		}
+		$update = $tvSeries->update(array(
+			'broadcast_program_id' => $putData->broadcastProgramId,
+			'main_cast' => $putData->mainCast
+		));
+		if (!$update) {
+			throw new Exception('TVSeries instance not updated', 500);
+		}
+
+		$bp = BroadcastProgram::findFirst($tvSeries->getBroadcastProgramId());
+		$bpArr = $bp->toArray();
+		unset($bpArr['id']);
+
+		return array('code' => 200, 'content' => array_merge($tvSeries->toArray(), $bpArr));
+	}
 }
