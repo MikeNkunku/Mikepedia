45856f09031b14d1b41871f6bf0463ba96964590
MikeNkunku <mikenkunku@gmail.com>
2016-03-28 14:42:50 +0200

    Fixed error in route associated with getList function

--- server/app/collections/AnimeCollection.php
+++ server/app/collections/AnimeCollection.php
@@ -15,7 +15,7 @@ $animeCollection->delete('/{animeId:[0-9]+}', 'delete');
 
 $animeCollection->get('/all', 'getAll');
 $animeCollection->get('/list', 'getValidList');
-$animeCollection->post('/list/{statusName:[a-z]+}', 'getList');
+$animeCollection->get('/list/{statusName:[a-z]+}', 'getList');
 $animeCollection->get('/{animeId:[0-9]+}/seasons', 'getSeasons');
 
 return $animeCollection;
